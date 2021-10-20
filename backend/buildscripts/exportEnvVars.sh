c9a481af26e92736745b8bbbd11a4670c5363c13
Jade McDermott <59776080+jade-e-mcdermott@users.noreply.github.com>
2021-10-20 00:21:47 -0700

    Add files via upload

---
+++ backend/buildscripts/exportEnvVars.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo 'export PATH="/home/jade/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
+
+echo 'export PATH=/home/jade/.pyenv/versions/3.7.2/bin:$PATH' >> /home/jade/.bash_profile && source /home/jade/.bash_profile
+
