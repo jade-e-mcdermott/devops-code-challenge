06c0e8615deb5d46b00d006910caa5640c697254
jade-e-mcdermott <jade.e.mcdermott@gmail.com>
2021-10-20 00:12:16 -0700

    broken eb environemnt setup and eb repo setup into seperate scripts.

--- backend/buildscripts/createEBEnv.sh
+++ backend/buildscripts/createEBEnv.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 cd ../
-# Create elastic beanstalk repo
-eb init --platform node.js --region us-west-2
 
 # Create Elastic Beanstalk env
 eb create lightfeather-backend
---
+++ backend/buildscripts/createEBRepo.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+cd ../
+# Create elastic beanstalk repo
+eb init --platform node.js --region us-west-2
